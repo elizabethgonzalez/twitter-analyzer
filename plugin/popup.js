50c1c322246a82bc8035c125cb00af4d06858ad8
Elizabeth Gonzalez <gonz023@usc.edu>
2020-10-16 21:38:51 -0700

    Update popup.js
    Add functionality to toggle, remove old code

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,24 +1,10 @@
-// let exampleCode = "if(document.body.style.backgroundColor == 'red'){document.body.style.backgroundColor = '';}else{document.body.style.backgroundColor ='red';}";
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//   var exampleButton = document.getElementById('example');
-//
-//   exampleButton.addEventListener('click', function() {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           {code: 'document.body.style.backgroundColor = "red";'});
-//     });
-//   }, false);
-// }, false);
-
 document.addEventListener('DOMContentLoaded', function() {
   var exampleButton = document.getElementById('example');
   exampleButton.addEventListener('click', function() {
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
-        tabs[0].id, {code: 'document.body.style.backgroundColor = "red";'}
+        tabs[0].id, {code: 'if(document.body.style.backgroundColor == "red"){document.body.style.backgroundColor = "";}else{document.body.style.backgroundColor ="red";}'}
       );
     });
   }, false);
