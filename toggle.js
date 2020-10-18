92282af17d61fd26dc70b83dfcc224ee32f91e80
Ashley Perez <asperez@usc.edu>
2020-10-18 07:44:45 -0700

    Delete toggle.js

--- toggle.js
+++
@@ -1,8 +0,0 @@
-if(document.querySelector('[aria-label="Tweet text"]').style.backgroundColor == "lemonchiffon"){
-  document.querySelector('[aria-label="Tweet text"]').style.backgroundColor = "";
-  document.querySelector('[data-testid="tweet"]').style.backgroundColor = "";
-}
-else{
-  document.querySelector('[aria-label="Tweet text"]').style.backgroundColor ="lemonchiffon";
-  document.querySelector('[data-testid="tweet"]').style.backgroundColor = "LightGreen";
-}
